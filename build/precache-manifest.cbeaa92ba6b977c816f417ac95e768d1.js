8c88af06affd3f84bc91483740ee5de0c1ca81ce
saran-mohan <54956294+saran-mohan@users.noreply.github.com>
2021-01-09 21:31:27 +0530

    Initial commit

---
+++ build/precache-manifest.cbeaa92ba6b977c816f417ac95e768d1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f521cd189bffda284fee5b48c67cd86d",
+    "url": "/resume-builder/index.html"
+  },
+  {
+    "revision": "b95c0e3c7fdb5e8dcd6f",
+    "url": "/resume-builder/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "f82130ef3dab021e7c68",
+    "url": "/resume-builder/static/css/main.04977f0c.chunk.css"
+  },
+  {
+    "revision": "b95c0e3c7fdb5e8dcd6f",
+    "url": "/resume-builder/static/js/2.317d3aed.chunk.js"
+  },
+  {
+    "revision": "f82130ef3dab021e7c68",
+    "url": "/resume-builder/static/js/main.30b69de5.chunk.js"
+  },
+  {
+    "revision": "fc7ccd356f4ed622e75d",
+    "url": "/resume-builder/static/js/runtime~main.fb5f9dcc.js"
+  },
+  {
+    "revision": "3266e00980a6c7d7ea600069d7994faf",
+    "url": "/resume-builder/static/media/IconCertificate.3266e009.svg"
+  },
+  {
+    "revision": "1e3ed0fff5059b87b7026f2396c43f24",
+    "url": "/resume-builder/static/media/IconExperience.1e3ed0ff.svg"
+  },
+  {
+    "revision": "04f2d54832042bac1b616a1bc5ce8162",
+    "url": "/resume-builder/static/media/IconGraduation.04f2d548.svg"
+  },
+  {
+    "revision": "841a1fa4d8598c4e6f8b268c141d81f3",
+    "url": "/resume-builder/static/media/IconIdea.841a1fa4.svg"
+  },
+  {
+    "revision": "7f860fb5bc00ab33bd273a25b5f5f496",
+    "url": "/resume-builder/static/media/IconLanguage.7f860fb5.svg"
+  },
+  {
+    "revision": "9bf65d2f47cebc79faa4db3907c680f2",
+    "url": "/resume-builder/static/media/IconPersonalInfo.9bf65d2f.svg"
+  },
+  {
+    "revision": "40785e2fda3d16d268fe1d683563cf51",
+    "url": "/resume-builder/static/media/IconSkills.40785e2f.svg"
+  }
+]);
